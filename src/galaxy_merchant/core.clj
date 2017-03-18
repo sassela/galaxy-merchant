35466eb4d6e60fb0d1ed2068b52ff77a3feee1b7
abby sassel <sassela@live.com>
2017-03-18 07:57:51 +0000

    Test numeral->value & string->keywords. Improve fn spec and names

--- src/galaxy_merchant/core.clj
+++ src/galaxy_merchant/core.clj
@@ -9,26 +9,26 @@
   [& args]
   (println "Hello, World!"))
 
+;; roman numerals
 (def ^:const NUMERAL_REGEX #"^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$")
-(def ^:const SYMBOLS {:M 1000, :V 5, :I 1, :L 50, :X 10, :C 100, :D 500})
-
-(def nblank? (complement str/blank?))
+(def ^:const SYMBOL_VALUES {:M 1000, :V 5, :I 1, :L 50, :X 10, :C 100, :D 500})
 
 (s/def ::numeral-ks
-  (->> SYMBOLS keys set))
+  (->> SYMBOL_VALUES keys set))
 
-(s/def ::numeral-strs
-  (->> SYMBOLS keys (map name) set))
+(s/def ::numeral-chars
+  (->> SYMBOL_VALUES keys (map name) set))
 
-(s/def ::numeral-symbol
-  (s/with-gen nblank? #(gen/fmap str/join (s/gen (s/+ ::numeral-strs)))))
+(s/def ::roman-numeral
+  (s/with-gen #(re-matches NUMERAL_REGEX %) #(gen/fmap str/join (gen/vector (s/gen ::numeral-chars) 1 10))))
 
-(s/def ::roman-numeral (s/and ::numeral-symbol #(re-matches NUMERAL_REGEX %)))
 
-(s/fdef keywords
-        :args ::numeral-symbol
-        :return (s/coll-of ::numeral-ks))
+(s/fdef string->keywords
+        :args (s/cat :input (s/and string? (complement str/blank?)))
+        :return (s/coll-of keyword?)
+        :fn #(= (count (-> % :args :input)) (count (:ret %))))
+
 
 (s/fdef numeral->value
-        :args ::roman-numeral
-        :ret nat-int?)
+        :args (s/cat :input ::roman-numeral)
+        :ret int?)
--- test/galaxy_merchant/core_test.clj
+++ test/galaxy_merchant/core_test.clj
@@ -1,7 +1,22 @@
 (ns galaxy-merchant.core-test
   (:require [clojure.test :refer :all]
+            [clojure.spec.test :as test]
             [galaxy-merchant.core :as c]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(->> (test/check `c/string->keywords) test/summarize-results)
+(deftest string->keywords-test
+  (testing "Given a string, returns a collection of keywords corresponding to characters"
+    (are [input actual] (= actual (c/string->keywords input))
+                        "X" '(:X)
+                        " DCCI " '(:D :C :C :I)
+                        "mccix" '(:M :C :C :I :X)
+                        "5" '(:5))))
+
+(->> (test/check `c/numeral->value) test/summarize-results)
+(deftest numeral->value-test
+  (testing "Given a single roman numeral, returns its value"
+    (are [input actual] (= actual (c/numeral->value input))
+                        "X" 10
+                        " DCCI " 701
+                        "MMV" 2005
+                        "mccix" 1209)))
